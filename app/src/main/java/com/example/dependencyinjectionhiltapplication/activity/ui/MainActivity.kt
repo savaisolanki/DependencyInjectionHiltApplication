1bde9ec1543d972d42ae616decb4e9b37b476eaa
savai_solanki <savai.solanki@brainvire.com>
2023-07-07 17:12:53 +0530

    Adding Hilt Dependency adding package name

--- app/src/main/java/com/example/dependencyinjectionhiltapplication/activity/ui/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionhiltapplication/activity/ui/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.example.dependencyinjectionhiltapplication
+package com.example.dependencyinjectionhiltapplication.activity.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.example.dependencyinjectionhiltapplication.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
